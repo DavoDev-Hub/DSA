b9ea9b067b79a54f473d454311fe5dc2fef07d82
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-20 20:39:15 -0600

    anagrams

---
+++ anagrams.py
@@ -0,0 +1,19 @@
+def anagrams(s1, s2):
+    return char_counter(s1) == char_counter(s2)
+
+
+def char_counter(s):
+    counter = {}
+    for char in s:
+        if char not in counter:
+            counter[char] = 0
+        counter[char] += 1
+
+    return counter
+
+
+s1 = "cats"
+s2 = "sact"
+
+result = anagrams(s1, s2)
+print(result)
---
+++ mostFrequentChar.py
@@ -0,0 +1,22 @@
+
+def most_frequent_char(s):
+
+
+
+# Posible solution
+# def most_frequent_char(s):
+#     count = {}
+#     for char in s:
+#         if char not in count:
+#             count[char] = 0
+#
+#         count[char] += 1
+#
+#     max_key = max(count, key=count.get)
+#
+#     return max_key
+#
+#
+# s = "beookeepeoro"
+# result = most_frequent_char(s)
+# print(result)

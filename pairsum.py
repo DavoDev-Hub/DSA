d156adf2bbccfaed5436f97b103be80910502a46
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-22 18:58:48 -0600

    pairsum

--- pairsum.py
+++ pairsum.py
@@ -1,18 +1,18 @@
-def pair_sum(numbers, target_sum):
-    prev = {}
-    for index, n in enumerate(numbers):
-        comp = target_sum - n
-        if comp in prev:
-            return (prev[comp], index)
-        prev[n] = index
+def pair_product(numbers, target_product):
+    prev_nums = {}
+    for index, num in enumerate(numbers):
+        complement = target_product / num
+        if complement in prev_nums:
+            return (prev_nums[complement], index)
+        prev_nums[num] = index
 
 
-result = pair_sum([3, 2, 5, 4, 1], 8)
+result = pair_product([3, 2, 5, 4, 1], 8)
 print(result)
 
 
 # brute force solution
 # for i in range (0, len(numbers)):
 #    for j in range(i+1, len(numbers))
-#      if numbers[i] = numbers[j] == target_sum
+#      if numbers[i] * numbers[j] == target_sum
 #        return(i,j)

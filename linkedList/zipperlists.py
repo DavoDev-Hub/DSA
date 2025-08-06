df7bba42493539f6f66b68a50debc56b1ef5f41d
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-05 23:15:18 -0600

    ziper list n(1) solution

--- linkedList/zipperlists.py
+++ linkedList/zipperlists.py
@@ -19,19 +19,42 @@ y.next = z
 
 
 def zipper_lists(head_1, head_2):
-    if head_1 is None and head_2 is None:
-        return None
+    tail = head_1
+    current1 = head_1.next
+    current2 = head_2
+    count = 0
+    while current1 is not None and current2 is not None:
+        if count % 2 == 0:
+            tail.next = current2
+            current2 = current2.next
+        else:
+            tail.next = current1
+            current1 = current1.next_1
+        tail += 1
+        count = +1
 
-    if head_2 is None:
-        return head_1
-
-    if head_1 is None:
-        return head_2
-
-    next_1 = head_1.next
-    next_2 = head_2.next
-
-    head_1.next = head_2
-    head_2.next = zipper_lists(next_1, next_2)
+    if current1 is None:
+        tail.next = current2
+    if current2 is None:
+        tail.next = current1
 
     return head_1
+
+
+# def zipper_lists(head_1, head_2):
+#     if head_1 is None and head_2 is None:
+#         return None
+#
+#     if head_2 is None:
+#         return head_1
+#
+#     if head_1 is None:
+#         return head_2
+#
+#     next_1 = head_1.next
+#     next_2 = head_2.next
+#
+#     head_1.next = head_2
+#     head_2.next = zipper_lists(next_1, next_2)
+#
+#     return head_1

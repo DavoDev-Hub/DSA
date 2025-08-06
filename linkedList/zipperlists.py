f4aff635be7c2cbf4421ae0e4ae1df95b991a7a6
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-05 22:51:43 -0600

    reversedList and zipperlist

---
+++ linkedList/zipperlists.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+a.next = b
+b.next = c
+
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+
+
+def zipper_lists(head_1, head_2):
+    if head_1 is None and head_2 is None:
+        return None
+
+    if head_2 is None:
+        return head_1
+
+    if head_1 is None:
+        return head_2
+
+    next_1 = head_1.next
+    next_2 = head_2.next
+
+    head_1.next = head_2
+    head_2.next = zipper_lists(next_1, next_2)
+
+    return head_1
